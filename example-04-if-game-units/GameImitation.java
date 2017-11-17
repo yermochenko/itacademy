d9f61d71e3af18f106ee67c90f5b7ea9299c9412
Sergey Yermochenko <yermochenko@gmail.com>
2017-11-17 23:31:08 +0300

    программа, имитирующая бой двух персонажей в игре

--- example-04-if-game-units/GameImitation.java
+++ example-04-if-game-units/GameImitation.java
@@ -45,6 +45,33 @@ public class GameImitation {
 			System.out.print("Здоровье 2-ого персонажа ");
 			System.out.print(health2);
 			System.out.println(" очков");
+			System.out.println("2-ой персонаж даёт сдачи 1-му персонажу");
+			if(attack2 > defense1) {
+				percent = (attack2 - defense1) * 0.05;
+				if(percent > 4) {
+					percent = 4;
+				}
+				totalDamage = damage2 * (1 + percent);
+			} else {
+				percent = (defense1 - attack2) * 0.03;
+				if(percent > 0.3) {
+					percent = 0.3;
+				}
+				totalDamage = damage2 * (1 - percent);
+			}
+			if(health1 > totalDamage) {
+				health1 = health1 - totalDamage;
+			} else {
+				health1 = 0;
+			}
+			if(health1 > 0) {
+				System.out.print("Здоровье 1-ого персонажа ");
+				System.out.print(health1);
+				System.out.println(" очков");
+				// 2-ой персонаж атакует 1-ый персонаж");
+			} else {
+				System.out.println("1-ый персонаж убит");
+			}
 		} else {
 			System.out.println("2-ой персонаж убит");
 		}
